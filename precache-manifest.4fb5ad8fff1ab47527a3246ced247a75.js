4c7c8d1d4662c83a56799590e84defebe6172b92
ne.andruh <ne.andruh@tensor.ru>
2020-03-25 16:43:59 +0700

    Updates

---
+++ precache-manifest.4fb5ad8fff1ab47527a3246ced247a75.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "669ed001d9851bd1aa5390993ae38d28",
+    "url": "/cosmos/index.html"
+  },
+  {
+    "revision": "687027f6a1055fc4313c",
+    "url": "/cosmos/static/css/2.512b24b6.chunk.css"
+  },
+  {
+    "revision": "819fd9377e41b8f01203",
+    "url": "/cosmos/static/css/main.6b0b8506.chunk.css"
+  },
+  {
+    "revision": "687027f6a1055fc4313c",
+    "url": "/cosmos/static/js/2.4e8b1752.chunk.js"
+  },
+  {
+    "revision": "2756ff2813abf767835a20625b4ab563",
+    "url": "/cosmos/static/js/2.4e8b1752.chunk.js.LICENSE"
+  },
+  {
+    "revision": "819fd9377e41b8f01203",
+    "url": "/cosmos/static/js/main.000612fb.chunk.js"
+  },
+  {
+    "revision": "c5508d6ab74943d09c7d",
+    "url": "/cosmos/static/js/runtime-main.c85a3727.js"
+  },
+  {
+    "revision": "c2c7784f70570c77fa9cae2981d11471",
+    "url": "/cosmos/static/media/mars.c2c7784f.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/cosmos/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/cosmos/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/cosmos/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/cosmos/static/media/slick.f97e3bbf.svg"
+  }
+]);
